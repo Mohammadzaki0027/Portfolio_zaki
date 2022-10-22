83e6ad2a43e769f0f6ead023a8ee002eaa22d032
Mohammadzaki0027 <zaki7875@gmail.com>
2022-10-22 17:58:00 +0530

    githubchanges

--- portfolio_zaki/src/Pages/Gitcale.jsx
+++ portfolio_zaki/src/Pages/Gitcale.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Styles/Git.css";
-
+import GitHubCalendar from "react-github-calendar";
+import ReactTooltip from "react-tooltip";
 const Gitcale = () => {
   const selectLastHalfYear = (contributions) => {
     const currentYear = new Date().getFullYear();
@@ -23,14 +24,20 @@ const Gitcale = () => {
       <h1>Github Info</h1>
       <div className="calendar">
         <h1>Graph</h1>
-        <img src="graph.jpg" alt="" />
+        <GitHubCalendar
+          username="Mohammadzaki0027"
+          year={new Date().getFullYear()}
+          blockSize={10}
+        >
+          <ReactTooltip delayShow={20} html/>
+        </GitHubCalendar>
         <h1>Streak</h1>
-
-        <img src="str.jpg" alt="" />
+      
+        <img src="https://github-readme-streak-stats.herokuapp.com?user=Mohammadzaki0027&theme=dark" alt="" />
         <h1>Statics</h1>
-        <img src="sta.jpg" alt="" />
+        <img src="https://github-readme-stats.vercel.app/api?username=Mohammadzaki0027&theme=highcontrast&show_icons=true" alt="" />
         <h1>Language Used</h1>
-        <img src="language.jpg" alt="" />
+        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Mohammadzaki0027" alt="" />
       </div>
     </div>
   );
